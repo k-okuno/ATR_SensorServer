893c03d02f57924784469f899079d8f2c0482556
K.OKUNO <keisuke.okuno@gmail.com>
2015-11-30 18:07:01 +0900

    add function dl_data-expect()
    to stop downloading data with EOF. this will reduce DL time and enable to DL any data size

--- func_dl_data.sh
+++ func_dl_data.sh
@@ -56,3 +56,59 @@ function dl_data()
     ) | telnet | col -b 2>&1 | tee -a ${data_file}
     echo "OK. Completed DL data" 2>&1 | tee -a ${logfile_name}
 }
+
+
+function dl_data-expect()
+{
+    local hostname=${1}
+    local port=${2}
+    local data_file=${3}
+    local logfile_name=${4}
+    
+    echo "Download data: (start in 7 sec)"  2>&1 | tee -a ${logfile_name}
+    echo "" 2>&1 | tee -a ${logfile_name}
+
+    expect -c "
+    set timeout -1
+    spawn telnet ${hostname} ${port}; sleep 1
+    expect \"\r\"
+    send \"readmemdata 1\r\"
+    expect \"EOF\"
+    send \"\035\r\"
+    expect \"telnet\>\"
+    send \"quit\n\"
+    " | col -b 2>&1 | tee -a ${data_file}
+}
+
+#    expect \"Escape character is \'\^\]\'\.\n\"
+#    send \"readmemdata 1\n\"
+
+    # expect \"\n\"
+    # send \"readmemdata 1\r\"
+
+# expect telnet>
+# sned \"quit\n\"
+
+
+#    expect Trying 127.0.0.1...  ; send \"getd\n\" 
+# expect \"$ \" ; send \"ls\r\"            # $ が出たら ls を打ち込む
+# expect \"$ \" ; send \"exit\r\"          # $ が出たら exit を打ち込む
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
