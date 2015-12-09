f70047668fae4be9b00191adddb41315448f44d7
K.OKUNO <keisuke.okuno@gmail.com>
2015-12-09 16:06:08 +0900

    add getopt and getopts template

--- template_get_args.sh
+++ template_get_args.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
-
-
-
 ################################
-# template
+# templates
 # getopts
 ################################
+
+##############################
+# getopt.sh
+##############################
+set -- 'getopt ad: $*'
+if [ $? != 0 ]; then
+    echo "Usage: $0 [-a] [-d dir]" 1>&2
+    exit 1
+fi
+for OPT in $*
+do
+    case $OPT in
+        -a) A_FLAG=1
+            shift
+            ;;
+        -d) B_ARG=$2
+            shift 2
+            ;;
+        --) shift
+            break
+            ;;
+    esac
+done
+
+
+##############################
+# getopts.sh
+##############################
+#!/bin/bash
+
 
 usage_exit() {
         echo "Usage: $0 [-a] [-d dir] item ..." 1>&2
@@ -27,6 +54,7 @@ do
 done
 
 shift $((OPTIND - 1))
+
 
 ################################
 # template
@@ -51,3 +79,4 @@ do
             ;;
     esac
 done
+
