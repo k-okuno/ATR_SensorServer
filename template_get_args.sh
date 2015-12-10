53a88b5dde63861456f07600e9d7b047982f05ca
Keisuke Okuno <keisuke.okuno@gmail.com>
2015-12-10 09:03:52 +0900

    add another template of getopt getopts
    for avoiding DIY error

--- template_get_args.sh
+++ template_get_args.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
 ################################
-# templates
-# getopts
+# templates of 
+# getopts/getopt
 ################################
 
 ##############################
@@ -28,55 +27,68 @@ do
 done
 
 
+
+##############################
+# another template
+# getopt.sh
+##############################
+OPT=`getopt -o ab:c --long long-a,  long-b:,long-c -- "$@"`
+if [ $? != 0 ] ; then
+    exit 1
+fi
+eval set -- "$OPT"
+
+while true
+do
+    case "$1" in
+    -a | --long-a)
+        # -a のときの処理
+        shift
+        ;;
+    -b | --long-b)
+        # -b のときの処理
+        shift 2
+        ;;
+    -c | --long-c)
+        # -c のときの処理
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *)
+        echo "Internal error!" 1>&2
+        exit 1
+        ;;
+    esac
+done
+
 ##############################
 # getopts.sh
 ##############################
-#!/bin/bash
-
-
-usage_exit() {
+function usage_exit()
+{
         echo "Usage: $0 [-a] [-d dir] item ..." 1>&2
         exit 1
 }
 
-while getopts ad:h OPT
-do
-    case $OPT in
-        a)  FLAG_A=1
-            ;;
-        d)  VALUE_D=$OPTARG
-            ;;
-        h)  usage_exit
-            ;;
-        \?) usage_exit
-            ;;
-    esac
-done
+function get_opts()
+{
+    while getopts ad:h OPT
+    do
+	case $OPT in
+            a)  FLAG_A=1
+		;;
+            d)  VALUE_D=$OPTARG
+		;;
+            h)  usage_exit
+		;;
+            \?) usage_exit
+		;;
+	esac
+    done
 
-shift $((OPTIND - 1))
-
-
-################################
-# template
-# getopt
-################################
-set -- 'getopt ad: $*'
-if [ $? != 0 ]; then
-    echo "Usage: $0 [-a] [-d dir]" 1>&2
-    exit 1
-fi
-for OPT in $*
-do
-    case $OPT in
-        -a) A_FLAG=1
-            shift
-            ;;
-        -d) B_ARG=$2
-            shift 2
-            ;;
-        --) shift
-            break
-            ;;
-    esac
-done
+    shift $((OPTIND - 1))
+}
 
