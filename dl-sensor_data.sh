89a46d2b9bbddf67ef95db7534f5ad75b6064aef
K.OKUNO <keisuke.okuno@gmail.com>
2015-12-10 23:18:46 +0900

    add function to set options.
    for flexibility

--- dl-sensor_data.sh
+++ dl-sensor_data.sh
@@ -6,39 +6,34 @@ NOW=${DATE}-${TIME}
 
 HOST="localhost"
 
-SCRIPT_NAME=${0}
+PROGNAME=$(basename $0)
+SCRIPT_NAME=$(basename $0)
+
 ALL_ARGS=$@
+
+# Default: interactive mode
+ALL_Y=FALSE
 
 # sleep time
 DURATION="0.2"
 
-#DEVID="1691"
-DEVID=${1}
+# Default device ID
+DEVID="-1"
 
 # Directory/Folder where the script is run.
 EXE_DIR=`pwd`
 
-# Directory/Folder to save data and log.
-EXP_DIR="./${DATE}_DEV${DEVID}"
 
 # Set connection means
 # CNCT=USB or CNCT=BT
+# Defualt is "USB"
 CNCT="USB"
 #CNCT="BT"
 
-# COM: BT -> 1, USB -> 2
-COM=2; if [ ${CNCT} = "BT" ]; then COM=1; fi
-
-# Port# : <BT=1 or USB=2><DEVID>
-PORT=${COM}${DEVID}
-
 # Data entry # to DL.
+# Default: 1
 WHICHDATA="1"
 #WHICHDATA="2"
-
-# File name
-FILENAME=${PORT}-${NOW}-${WHICHDATA}.csv
-LOGNAME=${PORT}-${NOW}-${WHICHDATA}.log
 
 
 #############
@@ -62,15 +57,131 @@ check_func_rtv()
 source ./func_cnct_check.sh
 source ./func_save_data-log.sh
 source ./func_dl_data.sh
+source ./func_if_num.sh
 
+
+##########################
+# print usage
+##########################
+usage() {
+    echo
+    echo "Usage: $PROGRAM [-y] [-h] [-c bt/usb] <DEVICE_ID> "
+    echo "  --debug. not implemented yet."            
+    echo "  -h, --help"
+    echo "  -y, --force-y"    
+    echo "  -c, --connection [bt/usb]"
+    echo "  -s, --sleep-time [sec]"
+    echo "  -d, --data-entry [1-40]"    
+    echo
+    exit 1
+}
+
+###############################
+# Check args and Set options
+###############################
+for OPT in "$@"	   
+do
+    case "$OPT" in
+        '-h'|'--help' )
+	    usage
+	    exit 1
+	    ;;
+        '-c'|'--connection' )
+	    if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+                echo "ERROR: $PROGNAME: option requires an argument -- $1" 1>&2
+                exit 1
+	    fi
+	    cnct="$2"
+	    if [ ${cnct} = "usb" ]; then
+		CNCT="USB"
+	    elif [ ${cnct} = "bt" ]; then
+		CNCT="BT"
+	    else
+		echo "ERROR: -c 'bt or usb'."
+		usage
+	    fi
+	    shift 2		
+	    ;;
+        '-y'|'--force-y' )
+	    ALL_Y=TRUE
+	    echo "ALL_Y: ${ALL_Y}"
+	    shift 1
+	    ;;
+	'-s'|'--sleep-time' )
+	    if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+                echo "ERROR: $PROGNAME: option requires an argument -- $1" 1>&2
+		usage
+	    fi
+	    if_num ${2} # this works only for "integer"
+	    if [ $? -ne 0 ]; then
+		echo "ERROR: not Numeric: $1 <= [Integer]" 1>&2
+		usage
+		exit 1
+	    else		
+		DURATION=${2}
+		echo "sleep time: ${DURATION}"
+	    fi
+	    shift 2		
+	    ;;
+	'-d'|'--data-entry' )
+	    if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]]; then
+                echo "ERROR: $PROGNAME: option requires an argument -- $1" 1>&2
+		usage
+	    fi
+	    if_num ${2} # this works only for "integer"
+	    if [ $? -ne 0 ]; then
+		echo "ERROR: not Numeric: $1 = [1-40]" 1>&2
+		usage
+		exit 1
+	    else		
+		WHICHDATA=${2}
+		echo "WHICH_DATA: ${WHICHDATA}"
+	    fi
+	    shift 2		
+	    ;;		
+        -*)
+	    echo "ERROR: $PROGNAME: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
+	    usage
+	    exit 1
+	    ;;
+        *)
+	    if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
+                #param=( ${param[@]} "$1" )
+                param+=( "$1" )
+		if_num ${param}
+		if [ $? -ne 0 ]; then
+		    echo "ERROR: not Numeric: $1 <= [Integer]" 1>&2
+		    usage
+		    exit 1
+		else
+		    DEVID=${param}
+		    echo "DEVID: ${DEVID}"
+		    shift 1
+		fi
+	    fi
+	    ;;
+    esac
+done
+
+if [ -z $param ]; then
+    echo "ERROR: $PROGNAME: too few arguments" 1>&2
+    usage
+    exit 1
+fi
+
+# setting up port.
+COM=2; if [ ${CNCT} = "BT" ]; then COM=1; fi		    
+PORT=${COM}${DEVID}
+# setting up File name
+FILENAME=${PORT}-${NOW}-${WHICHDATA}.csv
+LOGNAME=${PORT}-${NOW}-${WHICHDATA}.log
+# Directory/Folder to save data and log.
+EXP_DIR="./${DATE}_DEV${DEVID}"
 
 ##########################
 # main
 ##########################
-check_args ${@} 2>&1 | tee -a ${LOGNAME}
-check_func_rtv
-
-check_cnct ${HOST} ${PORT} | tee -a ${LOGNAME}
+check_cnct ${HOST} ${PORT} ${ALL_Y} 2>&1 | tee -a ${LOGNAME}
 check_func_rtv
 
 #dl_data ${HOST} ${PORT} | col -b 2>&1 | tee -a ${FILENAME}
